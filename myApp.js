399422fd058205519aa6d07fc308a084092bd86e
Israel Bautista <israel@hiplogiq.com>
2014-07-31 12:06:47 -0500

    quick update to v2

--- myApp.js
+++ myApp.js
@@ -7,13 +7,15 @@ angular.module('myApp', [])
     };
     return numbers;
     }
-    $scope.numbers = populateNumbers($scope.numberLimit);
+    // this cleans the need to do a calculation inside the template
     $scope.compute = function(a, b){
     	return a * b;
     };
+    // $watch listens for changes on the ng-model="numberLimit" and updates $scope.numbers
     $scope.$watch('numberLimit', function(limit){
     	$scope.numbers = populateNumbers(limit);
     });
+    // the $attrs reads the initial-number-limit from the template, it looks for initial-dash-spaced-name
     $scope.numberLimit = $attrs.initialNumberLimit || 10;
 });
 
